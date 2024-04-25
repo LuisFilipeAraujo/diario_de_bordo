2a4cc2a97e79872ac97feb90519872cbbd32ed17
Luis Filipe Alves Araujo <81663096+LuisFilipeAraujo@users.noreply.github.com>
2024-04-25 00:53:09 -0300

    testing routes login and menu. Added Login.ejs and Menu.ejs

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 const express = require('express');
+const path = require('path');
+
+const app=express();//inicializando o express
+
+app.set('view engine', 'ejs');//para configurar o ejs
+app.set('views', path.join(__dirname, 'views'));
+
+// Rota para a página de login
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+
+// Rota para a página "menu"
+app.get('/menu', (req, res) => {
+    res.render('menu');
+});
+
+app.listen(80, ()=>{
+    console.log('Working on port 80!')
+});
