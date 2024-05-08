fddc8f536d2adb256d76f519a6edc1f6e26f4058
Luis Filipe Alves Araujo <81663096+LuisFilipeAraujo@users.noreply.github.com>
2024-05-08 10:36:40 -0300

    veiculos.js now with a body model from the database

---
+++ model/Veiculos.js
@@ -0,0 +1,29 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('./database');
+
+const Veiculo = sequelize.define('Veiculo', {
+    Veiculo_ID: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        unique: true
+    },
+    marca: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    modelo: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    placa: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    setor_ID: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    }
+});
+
+module.exports = Veiculo;
