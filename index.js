1a53c18b933be54336d66df21a4167e002e553f3
Luis Filipe Alves Araujo <81663096+LuisFilipeAraujo@users.noreply.github.com>
2024-05-15 00:25:21 -0300

    changed paths and renamed files to adequate to the REST patterns

--- index.js
+++ index.js
@@ -22,38 +22,38 @@ app.get('/login', (req, res) => {
 });
 
 // Rota para a página "menu" do motorista
-app.get('/menuMotorista', (req, res) => {
-    res.render('menuMotorista');
+app.get('/motoristas/menuMotorista', (req, res) => {
+    res.render('motoristas/menuMotorista');
 });
 
 // Rota para a página "menu" do Adm
-app.get('/menuAdm', (req, res) => {
-    res.render('menuAdm');
+app.get('/motoristas/menuAdm', (req, res) => {
+    res.render('motoristas/menuAdm');
 });
 
 // Rota para a página "gerenciar-veiculos"
-app.get('/gerenciar-veiculos', (req, res) => {
-    res.render('gerenciar-veiculos');
+app.get('/veiculos/gerenciar-veiculos', (req, res) => {
+    res.render('veiculos/gerenciar-veiculos');
 });
 
 // Rota para a página "gerenciar-motoristas"
-app.get('/gerenciar-motoristas', (req, res) => {
-    res.render('gerenciar-motoristas');
+app.get('/motoristas/gerenciar-motoristas', (req, res) => {
+    res.render('motoristas/gerenciar-motoristas');
 });
 
 // Rota para a página "historico" do Adm
-app.get('/historico', (req, res) => {
-    res.render('historico');
+app.get('/viagens/historico', (req, res) => {
+    res.render('viagens/historico');
 });
 
 // Rota para a página adicionar-saida
-app.get('/adicionar-saida', (req, res) => {
-    res.render('adicionar-saida');
+app.get('/viagens/adicionar-saida', (req, res) => {
+    res.render('viagens/adicionar-saida');
 });
 
 // Rota para a página adicionar-chegada
-app.get('/adicionar-chegada', (req, res) => {
-    res.render('adicionar-chegada');
+app.get('/viagens/adicionar-chegada', (req, res) => {
+    res.render('viagens/adicionar-chegada');
 });
 
 
@@ -61,7 +61,7 @@ app.post('/login', (req, res) => {
     // Ainda precisa adicionar as credenciais de login e valida-las
     // Se as credenciais estiverem corretas, 
     // redireciona para a página de menu do motorista ou menu Adm(no momento só menuMotorista rs)
-    res.redirect('/menuMotorista');
+    res.redirect('motoristas/menuMotorista');
 });
 
 
