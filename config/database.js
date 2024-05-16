db2d22e52c7a189451c7b6e8d5e5d7305784db4d
Luis Filipe Alves Araujo <81663096+LuisFilipeAraujo@users.noreply.github.com>
2024-05-16 13:25:53 -0300

    bug fixed: removed the schema specification in config/database and removed function from model 'usuarios' so it exports 'Usuario' now

--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@ const Sequelize = require('sequelize');
 const sequelize = new Sequelize('diario_de_bordo', 'root', 'root123', {
     host: 'localhost',
     dialect: 'mysql',
-    schema: 'diario_de_bordo'
+    //schema: 'diario_de_bordo'
 });
 
 module.exports = sequelize;
--- models/usuarios.js
+++ models/usuarios.js
@@ -1,56 +1,57 @@
-const Sequelize = require('sequelize');
-module.exports = function(sequelize, DataTypes) {
-  return sequelize.define('usuarios', {
-    usuario_ID: {
-      autoIncrement: true,
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true
-    },
-    nome: {
-      type: DataTypes.STRING(255),
-      allowNull: false
-    },
-    tipo_Usuario: {
-      type: DataTypes.STRING(100),
-      allowNull: false
-    },
-    setor_ID: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      references: {
-        model: 'setor',
-        key: 'setor_ID'
-      }
-    },
-    login: {
-      type: DataTypes.STRING(255),
-      allowNull: false
-    },
-    senha: {
-      type: DataTypes.STRING(60),
-      allowNull: false
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
+const Usuario = sequelize.define('Usuario', {
+  usuario_ID: {
+    autoIncrement: true,
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true
+  },
+  nome: {
+    type: DataTypes.STRING(255),
+    allowNull: false
+  },
+  tipo_Usuario: {
+    type: DataTypes.STRING(100),
+    allowNull: false
+  },
+  setor_ID: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: 'setor',
+      key: 'setor_ID'
     }
-  }, {
-    sequelize,
-    tableName: 'usuarios',
-    timestamps: false,
-    indexes: [
-      {
-        name: "PRIMARY",
-        unique: true,
-        using: "BTREE",
-        fields: [
-          { name: "usuario_ID" },
-        ]
-      },
-      {
-        name: "fk_usuario_setor",
-        using: "BTREE",
-        fields: [
-          { name: "setor_ID" },
-        ]
-      },
-    ]
-  });
-};
+  },
+  login: {
+    type: DataTypes.STRING(255),
+    allowNull: false
+  },
+  senha: {
+    type: DataTypes.STRING(60),
+    allowNull: false
+  }
+}, {
+  tableName: 'usuarios',
+  timestamps: false,
+  indexes: [
+    {
+      name: "PRIMARY",
+      unique: true,
+      using: "BTREE",
+      fields: [
+        { name: "usuario_ID" },
+      ]
+    },
+    {
+      name: "fk_usuario_setor",
+      using: "BTREE",
+      fields: [
+        { name: "setor_ID" },
+      ]
+    },
+  ]
+});
+
+module.exports = Usuario;
