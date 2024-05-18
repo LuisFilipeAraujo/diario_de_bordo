a4dc5bdf903f40beb855a019701b2d58fab0bcd4
Luis Filipe Alves Araujo <81663096+LuisFilipeAraujo@users.noreply.github.com>
2024-05-18 18:28:31 -0300

    changed route to adicionar-veiculos

--- routes/veiculoRoutes.js
+++ routes/veiculoRoutes.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const veiculoController = require('../controllers/veiculoController');
 
 // Rota para adicionar um novo veículo
-router.post('/adicionar', veiculoController.adicionarVeiculo);
+router.post('/adicionar-veiculos', veiculoController.adicionarVeiculo);
 
 module.exports = router;
