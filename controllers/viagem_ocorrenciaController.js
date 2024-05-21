4bc3b4426ef93f81bf6f7cb93b26d406ce739e2c
Luis Filipe Alves Araujo <81663096+LuisFilipeAraujo@users.noreply.github.com>
2024-05-20 23:59:25 -0300

    added controller for viagem_ocorrencia

---
+++ controllers/viagem_ocorrenciaController.js
@@ -0,0 +1,15 @@
+const Viagem_ocorrencia = require('../models/viagem_ocorrencia');
+
+exports.adicionarViagem_ocorrencia = async (req, res) => {
+  try {
+    const { viagem_ID, ocorrencia_ID} = req.body;
+    
+  
+    const novaViagem_ocorrencia = await Viagem_ocorrencia.create({ viagem_ID, ocorrencia_ID });
+    
+    res.status(201).json({ message: 'Viagem_ocorrencia (tabela intermediária de n:n) adicionada com sucesso', viagem: novaViagem_ocorrencia });
+  } catch (error) {
+    console.error('Erro ao adicionar viagem_ocorrencia(tabela intermediária de n:n):', error);
+    res.status(500).json({ message: 'Erro ao adicionar viagem_ocorrencia(tabela intermediária de n:n)' });
+  }
+};
