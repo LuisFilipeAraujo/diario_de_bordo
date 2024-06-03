7a5a80b3a42b82d3b6f814241d71442459d7a63a
Luis Filipe Alves Araujo <81663096+LuisFilipeAraujo@users.noreply.github.com>
2024-06-02 23:24:40 -0300

    index controller created

---
+++ controllers/indexController.js
@@ -0,0 +1,18 @@
+
+const viagemController = require('./viagemController');
+const usuarioController = require('./usuarioController');
+const veiculoController = require('./veiculoController');
+const ocorrenciaController = require('./ocorrenciaController');
+const viagemOcorrenciaController = require('./viagem_ocorrenciaController');
+const setorController = require('./setorController');
+const loginController = require('./loginController')
+
+module.exports = {
+    viagemController,
+    usuarioController,
+    veiculoController,
+    ocorrenciaController,
+    viagemOcorrenciaController,
+    setorController,
+    loginController
+};
