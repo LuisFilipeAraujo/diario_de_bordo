ad15fd8346c5f69b93731ff800d09c5b242effc6
Luis Filipe Alves Araujo <81663096+LuisFilipeAraujo@users.noreply.github.com>
2024-06-14 13:15:46 -0300

    divided route adicionar viagem into adicionarSaida and adicionarChegada

--- routes/viagemRoutes.js
+++ routes/viagemRoutes.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const router = express.Router();
-//const viagemController = require('../controllers/viagemController');
-const { viagemController } = require('../controllers/indexController');
+const viagemController = require('../controllers/viagemController');
+//const { viagemController } = require('../controllers/indexController');
 
 
 // Rota para adicionar uma nova viagem
-router.post('/', viagemController.adicionarViagem);
+//router.post('/', viagemController.adicionarViagem);
+
+// Rota para adicionar os detalhes da saída (salvar em cookies)
+router.post('/adicionar-saida', viagemController.adicionarSaida);
+
+// Rota para adicionar detalhes da chegada
+router.post('/adicionar-chegada', viagemController.adicionarChegada);
 
 // Rota para listar todas as viagens
 router.get('/', viagemController.listarViagem);
