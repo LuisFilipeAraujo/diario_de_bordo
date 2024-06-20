a8cc42df9ab8d92c0025cfa04b6be8c2ea62da99
Luis Filipe Alves Araujo <81663096+LuisFilipeAraujo@users.noreply.github.com>
2024-06-20 01:42:08 -0300

    BR timezone: '-03:00'

--- config/database.js
+++ config/database.js
@@ -3,6 +3,7 @@ const Sequelize = require('sequelize');
 const sequelize = new Sequelize('diario_de_bordo', 'root', 'root123', {
     host: 'localhost',
     dialect: 'mysql',
+    timezone: '-03:00'
     //schema: 'diario_de_bordo'
 });
 
