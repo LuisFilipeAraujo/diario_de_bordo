013f6dec8c312317a21aae8cda7b11df23499454
Luis Filipe Alves Araujo <81663096+LuisFilipeAraujo@users.noreply.github.com>
2024-10-16 15:39:34 -0300

    refactor: dotenv added in gitignore

--- config/database.js
+++ config/database.js
@@ -1,10 +1,11 @@
 const Sequelize = require('sequelize');
+require('dotenv').config();
 
-const sequelize = new Sequelize('diario_de_bordo', 'root', 'root123', {
-    host: 'localhost',
-    dialect: 'mysql',
-    timezone: '-03:00'
-    //schema: 'diario_de_bordo'
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+    timezone: process.env.DB_TIMEZONE
+    
 });
 
 module.exports = sequelize;
